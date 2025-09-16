7da15cf3150fffed874e19dab456153d9cc7748a
lpraat <pratissolil@gmail.com>
2025-09-16 21:15:16 +0200

    Update python interface

--- crates/py_inbq/python/inbq/__init__.py
+++ crates/py_inbq/python/inbq/__init__.py
@@ -1,2 +1,2 @@
-from inbq._inbq import parse_sql, parse_sql_out_json
+from inbq._inbq import parse_sql as parse_sql, parse_sql_out_json as parse_sql_out_json
 from inbq.ast_nodes import *
--- crates/py_inbq/python/inbq/_inbq.pyi
+++ crates/py_inbq/python/inbq/_inbq.pyi
@@ -7,6 +7,17 @@ def parse_sql(sql: str) -> Ast:
         sql (str): the sql to parse
 
     Returns:
+        inbq.Ast: SQL abstract syntax tree
+    """
+    ...
+
+def parse_sql_out_json(sql: str) -> Ast:
+    """Parse a BigQuery SQL using the inbq Rust backend
+
+    Args:
+        sql (str): the sql to parse
+
+    Returns:
         str: JSON string representation of the SQL abstract syntax tree
     """
     ...
